024be482fcba86167504bd42db221acde2be6cfc
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-10-13 18:23:03 -0300

    yay

--- Selenium/src/main/java/AppSelenium.java
+++ Selenium/src/main/java/AppSelenium.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppSelenium {
@@ -6,15 +8,13 @@ public class AppSelenium {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\John Peter\\Desktop\\VeriVal2\\Selenium\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String baseUrl = "http://google.com.br";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
 
-        // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
 
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
 
+        WebElement searchBar = driver.findElement(By.name("q"));
+
+        System.out.println(searchBar);
         driver.close();
     }
 }
